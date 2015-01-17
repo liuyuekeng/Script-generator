8cf900ffb5aeecd827a122a16357a72df0a6a890
liuyuekeng <458482279@qq.com>
2015-01-17 15:07:30 +0000

    add row, add item evnet

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 ;(function(){
     var tpl = {
-        addCol : "<div class=\"addCol\"></div>",
-        addRow : "<div class=\"addRow\"></div>",
-        item : function () {
-            var item = document.createElement("div");
-            item.innerHTML = "aaaa";
+        addItemTpl : function () {
+            var item = document.createElement('span');
+            item.setAttribute('class', 'addItem');
+            item.innerHTML = "add item";
+            item.onclick = function (e) {
+                addItem(e);
+            }
             return item;
         },
-        row : "<div class=\"row\"></div>"
+        item : function () {
+            var item = document.createElement("input");
+            item.setAttribute('type', "text");
+            item.setAttribute('value', "aaaaa");
+            return item;
+        },
+        row : function () {
+            var item = document.createElement("div");
+            item.setAttribute('class', 'row');
+            return item;
+        }
     };
 
     function init () {
@@ -17,19 +29,38 @@
     function bindEvent () {
         var addItemBtn = document.querySelector(".addItem");
         var addRowBtn = document.querySelector(".addRow");
+        var buildBtn = document.getElementById("build");
         addItemBtn.onclick = function(e){
             addItem(e);
         };
-        addRowBtn.onclick = function(){
-            console.log('bbbbbb');
+        addRowBtn.onclick = function(e){
+            addRow(e);
         };
+        buildBtn.onclick = function(){
+            build();
+        }
     }
 
     function addItem (e) {
         e.target.parentNode.insertBefore(tpl.item(),e.target);
     }
 
-    function addRow () {
+    function addRow (e) {
+        var row = tpl.row();
+        row.appendChild(tpl.addItemTpl());
+        e.target.parentNode.insertBefore(row, e.target);
+    }
+
+    function build () {
+        var obj = {};
+        var rows = document.querySelectorAll(".row");
+        var rowLen = rows.length;
+        for (var i = 0; i < rowLen; i++) {
+            dealRowData(rows[i]);
+        }
+        function dealRowData (row) {
+            console.log(row);
+        }
     }
 
     init();
